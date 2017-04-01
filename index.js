971f406f78cd3ff3bfbbba5c6e7bc136bd103653
Leo Correa <lcorr005@gmail.com>
2017-04-01 11:06:39 -0400

    add help message

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const Alexa = require('alexa-sdk');
 const https = require('https');
 
-const welcomeMessage = "Welcome to Tronald Dump. A collection of quotes our 45th president has said. You can ask for a random quote. Would you like to hear one now?"
-const stopSkillMessage = "See you at Mar-a-Lago."
+const welcomeMessage = "Welcome to Tronald Dump. A collection of quotes our 45th president has said. You can ask for a random quote. Would you like to hear one now?";
+const stopSkillMessage = "See you at Mar-a-Lago.";
+const helpMessage = "How about asking for random quote or meme?";
 
 const handlers = {
   'LaunchRequest': function() {
     console.log("welcome");
-    this.emit(":ask", welcomeMessage)
+    this.emit(":ask", welcomeMessage, welcomeMessage);
   }
   'AMAZON.HelpIntent': function () {
-    this.emit(':ask', HelpMessage, HelpMessage);
+    this.emit(':ask', helpMessage, helpMessage);
   },
   'AMAZON.StopIntent': function () {
     this.shouldEndSession = true;
