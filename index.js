83e4a9d46b29d105c5162da31394be1d07f60dcc
Mathias Schilling <m@matchilling.com>
2017-05-13 01:39:14 +0100

    Some housekeeping

--- index.js
+++ index.js
@@ -1,65 +1,64 @@
-const Alexa = require('alexa-sdk');
-const https = require('https');
-
-const welcomeMessage = "Welcome to Tronald Dump. A collection of quotes our 45th president has said. You can ask for a random quote or meme. Which one would you like?";
-const stopSkillMessage = "See you at Mar-a-Lago.";
-const helpMessage = "How about asking for random quote or meme? We don't have alternative facts here.";
+const Alexa = require('alexa-sdk'),
+      https = require('https'),
+      welcomeMessage = 'Welcome to Tronald Dump. A collection of quotes our 45th president has said. You can ask for a random quote or meme. Which one would you like?',
+      stopSkillMessage = 'See you at Mar-a-Lago.',
+      helpMessage = 'How about asking for random quote or meme? We don\'t have alternative facts here.';
 
 function getRandomQuote(cb) {
   const options = {
-    host: 'api.tronalddump.io',
-    path: '/random/quote',
-    method: 'GET',
-    headers: {
-      'user-agent': 'Tronald Dump Alexa',
-      'accept': 'application/hal+json'
+    host    : 'api.tronalddump.io',
+    path    : '/random/quote',
+    method  : 'GET',
+    headers : {
+      'user-agent' : 'Tronald Dump Alexa',
+      accept       : 'application/hal+json'
     }
   };
   return https.get(options, (res) => {
     res.on('data', (d) => {
-      resp = JSON.parse(d)
-      cb(resp);
+      cb(JSON.parse(d));
     });
   });
 }
 
 const handlers = {
-  'LaunchRequest': function() {
-    console.log("welcome");
-    this.emit(":ask", welcomeMessage, welcomeMessage);
+  LaunchRequest() {
+    this.emit(':ask', welcomeMessage, welcomeMessage);
   },
-  'GetRandomQuote': function() {
+  GetRandomQuote() {
     getRandomQuote((resp) => {
-      const quote = resp.value;
-      const source = resp._embedded.source[0];
-      const cardMessage = `${quote}\n\nYou can find the source of this rant here: ${source.url}`;
-      this.emit(":tellWithCard", quote, "Quote Source", cardMessage);
+      const quote = resp.value,
+            source = resp._embedded.source[0],
+            cardMessage = `${quote}\n\nYou can find the source of this rant here: ${source.url}`;
+
+      this.emit(':tellWithCard', quote, 'Quote Source', cardMessage);
     });
   },
-  'GetRandomMeme': function() {
+  GetRandomMeme() {
     // TODO: This intent has been disabled since the Alexa app displays this image as not found
-    const url = "https://api.tronalddump.io/random/meme";
-    const imgObj = {
-      smallImageUrl: url,
-      largeImageUrl: url
-    };
-    this.emit(":tellWithCard", "Check the alexa app to see your meme.", "Tronald Dump", "Enjoy your meme!", imgObj);
+    const url = 'https://api.tronalddump.io/random/meme',
+          imgObj = {
+            smallImageUrl : url,
+            largeImageUrl : url
+          };
+
+    this.emit(':tellWithCard', 'Check the alexa app to see your meme.', 'Tronald Dump', 'Enjoy your meme!', imgObj);
   },
-  'AMAZON.HelpIntent': function () {
+  'AMAZON.HelpIntent' : function () {
     this.emit(':ask', helpMessage, helpMessage);
   },
-  'AMAZON.StopIntent': function () {
+  'AMAZON.StopIntent' : function () {
     this.shouldEndSession = true;
     this.emit(':tell', stopSkillMessage, stopSkillMessage);
   },
-  'AMAZON.CancelIntent': function () {
+  'AMAZON.CancelIntent' : function () {
     this.shouldEndSession = true;
     this.emit(':tell', stopSkillMessage, stopSkillMessage);
-  },
-}
+  }
+};
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context) {
   const alexa = Alexa.handler(event, context);
   alexa.registerHandlers(handlers);
   alexa.execute();
-}
+};
